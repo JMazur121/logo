6b940ad37106ed4530e19525843a34ede0886523
JMazur121 <mrsilviril@gmail.com>
2018-11-28 00:27:27 +0100

    New methods for stream agent

--- lexer/src/main/java/agent/CharacterStreamAgent.java
+++ lexer/src/main/java/agent/CharacterStreamAgent.java
@@ -1,8 +1,10 @@
 package agent;
 
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
+import java.util.Optional;
 
 public class CharacterStreamAgent {
 
@@ -11,14 +13,28 @@ public class CharacterStreamAgent {
 	private int bufferedPosition;
 	private int inStreamPosition;
 	private char buffer;
-	private boolean isBufferEmpty;
+	private boolean bufferContainsChar;
+	private boolean isCorrupted;
 
 	public CharacterStreamAgent() {
+		resetAgent();
+	}
+
+	public void resetAgent() {
 		streamReader = null;
 		currentStream = null;
-		bufferedPosition = 0;
-		inStreamPosition = 0;
-		isBufferEmpty = true;
+		bufferedPosition = 1;
+		inStreamPosition = 1;
+		bufferContainsChar = true;
+		isCorrupted = false;
+	}
+
+	public int getBufferedPosition() {
+		return bufferedPosition;
+	}
+
+	public boolean isCorrupted() {
+		return isCorrupted;
 	}
 
 	public void handleStream(InputStream inputStream) {
@@ -26,4 +42,29 @@ public class CharacterStreamAgent {
 		currentStream = inputStream;
 	}
 
+	public Optional<Character> bufferAndGetChar() {
+		if (isCorrupted)
+			return Optional.empty();
+		if (bufferContainsChar)
+			return Optional.of(buffer);
+		try {
+			int character = streamReader.read();
+			inStreamPosition++;
+			if (character == -1)
+				return Optional.empty();
+			bufferContainsChar = true;
+			char newChar = (char)character;
+			return Optional.of(newChar);
+		} catch (IOException e) {
+			isCorrupted = true;
+			return Optional.empty();
+		}
+	}
+
+	public void commitBufferedChar() {
+		if (bufferContainsChar) {
+			bufferContainsChar = false;
+			bufferedPosition++;
+		}
+	}
 }
