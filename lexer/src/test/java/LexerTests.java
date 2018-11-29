fb9199e8fc14da7c0e0c7d1018529fa7ebee9ef3
JMazur121 <mrsilviril@gmail.com>
2018-11-29 02:49:35 +0100

    More tests

--- lexer/src/test/java/LexerTests.java
+++ lexer/src/test/java/LexerTests.java
@@ -75,6 +75,23 @@ public class LexerTests {
 	}
 
 	@Test
+	public void nextToken_singleIdentifierWithDigits_returnsIdentifierToken() {
+		//before
+		lexer.restart();
+		ByteArrayInputStream is = new ByteArrayInputStream("a123".getBytes());
+		lexer.handleStream(is);
+		//when
+		Optional<Token> token = lexer.nextToken();
+		//then
+		assertThat(token.isPresent()).isTrue();
+		Token identifierToken = token.get();
+		assertThat(identifierToken.getTokenType()).isEqualTo(T_IDENTIFIER);
+		assertThat(identifierToken.getLine()).isEqualTo(1);
+		assertThat(identifierToken.getPositionInLine()).isEqualTo(1);
+		assertThat(identifierToken.getAbsolutePosition()).isEqualTo(1);
+	}
+
+	@Test
 	public void nextToken_streamWithSingleNumericConstant_returnsNumericConstantToken() {
 		//before
 		lexer.restart();
@@ -89,7 +106,6 @@ public class LexerTests {
 		assertThat(numericToken.getLine()).isEqualTo(1);
 		assertThat(numericToken.getPositionInLine()).isEqualTo(1);
 		assertThat(numericToken.getAbsolutePosition()).isEqualTo(1);
-
 	}
 
 	@Test
@@ -107,10 +123,64 @@ public class LexerTests {
 	}
 
 	@Test
+	public void nextToken_streamWithSingleOperator_returnsOperatorToken() {
+		//before
+		lexer.restart();
+		ByteArrayInputStream is = new ByteArrayInputStream("+".getBytes());
+		lexer.handleStream(is);
+		//when
+		Optional<Token> token = lexer.nextToken();
+		//then
+		assertThat(token.isPresent()).isTrue();
+		Token plusToken = token.get();
+		assertThat(plusToken.getTokenType()).isEqualTo(T_ARITHMETIC_ADDITIVE_PLUS);
+		assertThat(plusToken.getLine()).isEqualTo(1);
+		assertThat(plusToken.getPositionInLine()).isEqualTo(1);
+		assertThat(plusToken.getAbsolutePosition()).isEqualTo(1);
+	}
+
+	@Test
+	public void nextToken_streamWithWhitespaces_returnsTwoTokens() {
+		//before
+		lexer.restart();
+		ByteArrayInputStream is = new ByteArrayInputStream("def kwadrat".getBytes());
+		lexer.handleStream(is);
+		//when
+		Optional<Token> defToken = lexer.nextToken();
+		Optional<Token> identifierToken = lexer.nextToken();
+		//then
+		assertThat(defToken.isPresent()).isTrue();
+		assertThat(identifierToken.isPresent()).isTrue();
+		Token def = defToken.get();
+		Token identifier = identifierToken.get();
+		assertThat(def.getTokenType()).isEqualTo(T_KEYWORD_PROC_DEFINITION);
+		assertThat(def.getAbsolutePosition()).isEqualTo(1);
+		assertThat(def.getLine()).isEqualTo(1);
+		assertThat(def.getPositionInLine()).isEqualTo(1);
+		assertThat(identifier.getTokenType()).isEqualTo(T_IDENTIFIER);
+		assertThat(identifier.getAbsolutePosition()).isEqualTo(5);
+		assertThat(identifier.getLine()).isEqualTo(1);
+		assertThat(identifier.getPositionInLine()).isEqualTo(5);
+	}
+
+	@Test
 	public void nextToken_streamWithLineComment_returnsETXToken() {
 		//before
 		lexer.restart();
 		ByteArrayInputStream is = new ByteArrayInputStream("#comment".getBytes());
+		lexer.handleStream(is);
+		//when
+		Optional<Token> token = lexer.nextToken();
+		//then
+		assertThat(token.isPresent()).isTrue();
+		assertThat(token.get().getTokenType()).isEqualTo(T_CONTROL_ETX);
+	}
+
+	@Test
+	public void nextToken_streamWithBlockComment_returnsETXToken() {
+		//before
+		lexer.restart();
+		ByteArrayInputStream is = new ByteArrayInputStream("{sdad\ndasd\nsadd\n}".getBytes());
 		lexer.handleStream(is);
 		//when
 		Optional<Token> token = lexer.nextToken();
